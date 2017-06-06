24f53fa0a060cf24875caa0a0c0b49a041363f46
V-2Schneider <jodieczeci@gmail.com>
2017-06-06 15:01:03 +0200

    Add project files.

---
+++ SpaceCrusader2/Engine_visual.cpp
@@ -0,0 +1,17 @@
+//funkcja do rysowania wszystkiego
+
+//poni¿sze powinny mieæ jakiœ for
+
+//do rysowania przeciwników
+
+//do rysowania gracza
+
+//do rysowania t³a
+
+//rysowanie przeszkód
+
+//rysowanie menu
+
+//rysowanie gui
+
+//rysowanie pocisków
---
+++ SpaceCrusader2/Main.cpp
@@ -0,0 +1,52 @@
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics.hpp>
+
+class Game {
+public:
+	Game();
+	void run();
+
+private: 
+	void processEvents();
+	void update();
+	void render();
+
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+};
+
+Game::Game() : mWindow(sf::VideoMode(640, 480), "SFML Application"), mPlayer() {
+	mPlayer.setRadius(40.f);
+	mPlayer.setPosition(100.f, 100.f);
+	mPlayer.setFillColor(sf::Color::Cyan);
+}
+
+void Game::run() {
+	while (mWindow.isOpen()) {
+		processEvents();
+		update();
+		render();
+	}
+}
+
+void Game::processEvents(){
+	sf::Event event;
+	while (mWindow.pollEvent(event)){
+		if (event.type == sf::Event::Closed)
+			mWindow.close();
+	}
+}
+
+void Game::update(){}
+
+void Game::render(){
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
+}
+
+int main() {
+	Game game;
+	game.run();
+	return 0;
+}
