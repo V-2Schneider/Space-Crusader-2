62ceadb12da85f89d84c0211e8bd96edc16b754b
V-2Schneider <jodieczeci@gmail.com>
2017-06-07 19:06:02 +0200

    I changed the window title! Small change to test commits

--- SpaceCrusader2/Main.cpp
+++ SpaceCrusader2/Main.cpp
@@ -15,7 +15,7 @@ private:
 	sf::CircleShape mPlayer;
 };
 
-Game::Game() : mWindow(sf::VideoMode(640, 480), "SFML Application"), mPlayer() {
+Game::Game() : mWindow(sf::VideoMode(640, 480), "Space Crusader 2"), mPlayer() {
 	mPlayer.setRadius(40.f);
 	mPlayer.setPosition(100.f, 100.f);
 	mPlayer.setFillColor(sf::Color::Cyan);
