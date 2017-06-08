e76b52a207bfedad36de1bce0cf7c4db81ca953f
V-2Schneider <jodieczeci@gmail.com>
2017-06-08 18:47:15 +0200

    Cleaned up the code a bit, moved Game class to a different file

---
+++ SpaceCrusader2/Game.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics.hpp>
+
+class Game {
+public:
+	Game();
+	void run();
+
+private:
+	void processEvents();
+	void update(sf::Time);
+	void render();
+	void handlePlayerInput(sf::Keyboard::Key, bool);
+
+	bool mIsMovingUp;
+	bool mIsMovingDown;
+	bool mIsMovingLeft;
+	bool mIsMovingRight;
+
+	static const float Game::PlayerSpeed;
+	static const sf::Time Game::TimePerFrame;
+
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+};
+
+#endif 
