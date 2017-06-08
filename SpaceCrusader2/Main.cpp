0096e7e5c3046757b5fcde51c0b864d7b4affd95
V-2Schneider <jodieczeci@gmail.com>
2017-06-08 17:56:37 +0200

    Moving on with the tutorial- for now it's nothing special, I'm still doing what I'm told. Added movement, learned a bit about Vector2f and managing frame rate

--- SpaceCrusader2/Main.cpp
+++ SpaceCrusader2/Main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/System/Time.hpp>
 #include <SFML/Graphics.hpp>
 
+
+
 class Game {
 public:
 	Game();
@@ -8,8 +10,18 @@ public:
 
 private: 
 	void processEvents();
-	void update();
+	void update(sf::Time);
 	void render();
+	void handlePlayerInput(sf::Keyboard::Key, bool);
+
+	bool mIsMovingUp;
+	bool mIsMovingDown;
+	bool mIsMovingLeft;
+	bool mIsMovingRight;
+
+	const float Game::PlayerSpeed = 100.f;
+	const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
+
 
 	sf::RenderWindow mWindow;
 	sf::CircleShape mPlayer;
@@ -22,22 +34,61 @@ Game::Game() : mWindow(sf::VideoMode(640, 480), "Space Crusader 2"), mPlayer() {
 }
 
 void Game::run() {
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
 	while (mWindow.isOpen()) {
 		processEvents();
-		update();
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > TimePerFrame) {
+			timeSinceLastUpdate -= TimePerFrame;
+			processEvents();
+			update(TimePerFrame);
+		}
 		render();
 	}
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
+	if (key == sf::Keyboard::W)
+		mIsMovingUp = isPressed;
+	else if (key == sf::Keyboard::S)
+		mIsMovingDown = isPressed;
+	else if (key == sf::Keyboard::A)
+		mIsMovingLeft = isPressed;
+	else if (key == sf::Keyboard::D)
+		mIsMovingRight = isPressed;
 }
 
 void Game::processEvents(){
 	sf::Event event;
 	while (mWindow.pollEvent(event)){
-		if (event.type == sf::Event::Closed)
+		switch (event.type) {
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+		case sf::Event::Closed:
 			mWindow.close();
+			break;
+		}
 	}
 }
 
-void Game::update(){}
+void Game::update(sf::Time deltaTime){
+	sf::Vector2f movement(0.f, 0.f);
+	if (mIsMovingUp)
+		movement.y -= PlayerSpeed;
+	if (mIsMovingDown)
+		movement.y += PlayerSpeed;
+	if (mIsMovingLeft)
+		movement.x -= PlayerSpeed;
+	if (mIsMovingRight)
+		movement.x += PlayerSpeed;
+
+	mPlayer.move(movement * deltaTime.asSeconds());
+}
 
 void Game::render(){
 	mWindow.clear();
